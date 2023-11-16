c1b61260b0e86de40df365e2a5a32f4464ddd66f
ThinhNgo767 <thinhngo767@gmail.com>
2023-11-16 21:52:53 +0700

    gitignore

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
-const {createFile ,readFile} = require("./modules/fileSystem")
+const { createFile, readFile } = require("./modules/fileSystem");
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-let fileName = "text.txt"
-let fileContent = "Hello MindX!"
+let fileName = "text.txt";
+let fileContent = "Hello MindX!";
 
-createFile(fileName,fileContent)
+createFile(fileName, fileContent);
 
-readFile(fileName)
+readFile(fileName);
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Hello, this is homepage</h1>`)
-  })
-  
-  app.listen(3001, () => {
-    console.log("Example app listening on port 3001")
-  })
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello, this is homepage</h1>`);
+});
 
+app.listen(3001, () => {
+  console.log("Example app listening on http://localhost:3001");
+});
